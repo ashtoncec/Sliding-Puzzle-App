3b1381a143e3400f23c41f217dbde51dd23c23ce
ashtoncec <123290547+ashtoncec@users.noreply.github.com>
2024-02-27 15:02:40 -0600

    -updated the names of the png images for clarity

---
+++ Ind02_Cecil_Ashton/DrawingView.swift
@@ -0,0 +1,20 @@
+//
+//  DrawingView.swift
+//  Ind02_Cecil_Ashton
+//
+//  Created by Ashton Cecil on 2/27/24.
+//
+
+import UIKit
+
+class DrawingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
