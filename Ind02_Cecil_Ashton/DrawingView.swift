2412765da8dbb87385de927ae309e9639d354b5d
ashtoncec <123290547+ashtoncec@users.noreply.github.com>
2024-02-27 21:27:15 -0600

    -added background color

--- Ind02_Cecil_Ashton/DrawingView.swift
+++ Ind02_Cecil_Ashton/DrawingView.swift
@@ -11,19 +11,9 @@ class DrawingView: UIView {
 
     var lake: UIImage? = UIImage(named: "lakeLouiseOne.png")
     
-    func drawLakeLouise(){
-        guard let lake = self.lake else{
-            print("**** Cannot find Lake! ****")
-            return
-        }
     
-    let p = CGPoint(
-    x: (self.bounds.size.width - lake.size.width) / 2,
-    y: (self.bounds.size.height - lake.size.height) / 2)
-        lake.draw(at: p)
-    }
     
     override func draw(_ rect: CGRect){
-        drawLakeLouise()
+        
     }
 }
